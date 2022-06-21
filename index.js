736cd852c21d3aa528f314d76b68abf1fafc300b
Alexandre <carronalexandre77@gmail.com>
2022-06-21 20:53:44 +0200

    test

---
+++ index.js
@@ -0,0 +1,11 @@
+let x = 2;
+let y = 5;
+
+if (x < y) {
+  console.log("c'est vrai");
+  console.log("c'est vrai");
+  console.log("c'est vrai");
+  
+  console.log("c'est vrai");
+  console.log("c'est vrai");
+} else
